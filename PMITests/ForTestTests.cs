8111a2620143a46f533da1cc388586052033067c
Robson10 <mucharobert4@wp.pl>
2016-11-24 13:08:56 +0100

    init commit

---
+++ PMITests/ForTestTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMI.Tests
+{
+    [TestClass()]
+    public class ForTestTests
+    {
+
+
+        [TestMethod()]
+        public void addTest()
+        {
+            ForTest x = new ForTest();
+            int result = x.add(1, 1);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void divTest()
+        {
+            ForTest x = new ForTest();
+            double result = x.div(3, 2);
+            Assert.AreEqual(1.5, result);
+        }
+    }
+}
